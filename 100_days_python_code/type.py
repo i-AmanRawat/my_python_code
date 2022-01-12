bfefb1820c07f2bf18a89911031fbf26bc0168e6
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-01-12 21:48:19 +0530

    Create type.py

---
+++ 100_days_python_code/type.py
@@ -0,0 +1,27 @@
+lst1 = str([])
+print(type(lst1))
+print(lst1)
+
+print("\n")
+
+lst = []
+print(type(lst))
+print(lst)
+
+print("\n")
+
+dict1 = {}
+print(type(dict1))
+print(dict1)
+
+print("\n")
+
+dict2 = str({})
+print(type(dict2))
+print(dict2)
+
+print("\n")
+
+dict3 = list({})
+print(type(dict3))
+print(dict3)
