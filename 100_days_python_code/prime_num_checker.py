a4e76eae6294534732ef597efbd104c7f3a2686f
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-03-10 10:58:03 +0530

    Update prime_num_checker.py

--- 100_days_python_code/prime_num_checker.py
+++ 100_days_python_code/prime_num_checker.py
@@ -3,8 +3,7 @@
 def prime_checker (number ) :
   if (number ==2 or number ==3 or number ==5 or number ==7 ):
     print("It's a prime number.")
-
-    print ("It's not a prime number.")
+    
   elif ((number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0)):
     print("It's not a prime number")
   else :
