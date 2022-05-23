f8ec6bfe1141fbb0484802e552895c66bb022173
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-05-23 21:48:26 +0530

    Create turtle.py

---
+++ 100_days_python_code/16_oop/turtle.py
@@ -0,0 +1,8 @@
+from turtle import Turtle , Screen 
+turtur = Turtle()       #creating an object with help of a Turtle class
+my_screen = Screen()
+turtur.shape("turtle")  #method of object
+turtur.color("coral")   #method
+turtur.forward(100)         #move forward by 100 paces
+my_screen.exitonclick()     #method
+print('hello')
