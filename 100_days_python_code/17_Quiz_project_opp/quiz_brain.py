896913dcc23a317c27e53b1a84c0439e138f2fec
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-05-28 17:54:34 +0530

    Create quiz_brain.py

---
+++ 100_days_python_code/17_Quiz_project_opp/quiz_brain.py
@@ -0,0 +1,34 @@
+class QuizBrain :
+        def __init__ (self , q_list):
+            self.question_number = 0
+            self.questions_list = q_list
+            self.score=0
+
+        def next_question(self):
+                self.current_question = self.questions_list[self.question_number]
+                self.question_number+=1
+                return input(f"Q.{self.question_number}: {self.current_question.text} (True/False)?: ")
+
+        def still_has_question (self):
+            return self.question_number < len(self.questions_list)
+
+        def check_answer(self):
+            return self.next_question() == self.current_question.answer
+
+
+        def score (self):
+            if self.check_answer() == "True" :
+                self.score+=1
+                print("You got it right!")
+                print("The correct answer was: True.")
+                print("Your current score is: ", self.score, "/", self.question_number)
+            else:
+                print("You got it wrong!")
+                print("The correct answer was: ", self.current_question.answer)
+                print("Your current score is: ", self.score, "/", self.question_number)
+
+
+
+# TODO : asking the questions
+# TODO : checking if the answer was correct
+# TODO : checking if we're the end of the quiz
