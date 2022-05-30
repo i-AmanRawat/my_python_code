4d5f0f696b969459009831dc4d288bee980e60b7
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-05-30 12:54:31 +0530

    Update draw_different_shape.py

--- 100_days_python_code/18_turtle and GUI/draw_different_shape.py
+++ 100_days_python_code/18_turtle and GUI/draw_different_shape.py
@@ -3,16 +3,20 @@ import random
 
 def draw_shape(sides):
     angle = 360/sides
-    timmy.forward(100)
-    timmy.right(angle)
+    for i in range(sides):
+        timmy.forward(100)
+        timmy.right(angle)
 
 def colour() :
     list_of_color = ['dark magenta', 'black', 'dark red', 'dark slate grey', 'maroon', 'dark slate blue']
     return random.choice(list_of_color)
 
 timmy= Turtle()
+timmy= Screen()
+timmy.exitonclick()
+timmy.pen(pensize=3, speed= 10)
 for side_n in range(3,11):
     timmy.color(colour())
     draw_shape(side_n)
-    
-    
+
+
