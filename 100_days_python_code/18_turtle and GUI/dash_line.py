9cbb94d4e1fe1c5cd84a8ef6de5dfafac4b7857d
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-05-30 11:14:44 +0530

    Update dash_line.py

--- 100_days_python_code/18_turtle and GUI/dash_line.py
+++ 100_days_python_code/18_turtle and GUI/dash_line.py
@@ -1,1 +1,30 @@
+"""
+import turtle
+
+from turtle import Turtle   #for and import --> to include specific method
+
+from turtle import *    # * --> astersic is used to include all the methods of the turtle
+
+import turtle as t  #aaliasing the turtle module
+
+"""
+from turtle import Turtle , Screen
+jack = Turtle()     #defining object of turtle classs
+# jack.shape("turtle")        #for shapes
+jack.color("dark green")    #for color
+# jack.dot(50)    #dot(size , color) for dash lines
+# jack.setfillcapacity(50)
+# jack.forward(100)   #direction of motion
+for i in range (30):
+    jack.pendown()
+    jack.forward(5)
+    jack.penup()
+    jack.forward(5)
+
+
+display = Screen()
+display.exitonclick()
+
+
+
 
